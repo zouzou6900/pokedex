5a80b39f2b9515fc8d1e6580fdea15bd487febe4
heazyh <xtralys@gmail.com>
2023-11-28 14:23:01 +0100

    commit modif css

--- atelier-pokedex/assets/js/components/header.js
+++ atelier-pokedex/assets/js/components/header.js
@@ -1,18 +1,18 @@
 export default class Header extends HTMLElement {
-    connectedCallback() {
-      const current = this.getAttribute("current");
-      this.innerHTML = `<header>
+  connectedCallback() {
+    const current = this.getAttribute('current');
+    this.innerHTML = `<header>
       <div class="logo"><img src="public/img/logo-banner.png" alt="logo pokemon"></div>
       <nav>
         <ul>
             <li><a style="color: ${
-                current === "world" ? "green" : "red"
+              current === 'world' ? '#ffcb08' : 'white'
             }" href="./">World</a></li>
             <li><a style="color: ${
-                current === "pokedex" ? "green" : "red"
+              current === 'pokedex' ? '#ffcb08' : 'white'
             }" href="./pokedex.html">Pokedex</a></li>
             <li><a style="color: ${
-                current === "history" ? "green" : "red"
+              current === 'history' ? '#ffcb08' : 'white'
             }" href="./history.html">Historique</a></li>
         </ul>
       </nav>
@@ -21,5 +21,5 @@ export default class Header extends HTMLElement {
           <p class="notCatch">20</p>
       </div>
       </header>`;
-    }
+  }
 }
