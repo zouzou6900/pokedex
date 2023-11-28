a866d8e37acbfaaba958de2cfa055e1eb8163165
zouzou6900 <meersman_amaury@hotmail.com>
2023-11-28 16:36:57 +0100

    init pokedex

--- atelier-pokedex/assets/js/component.js
+++ atelier-pokedex/assets/js/component.js
@@ -1,7 +1,9 @@
 import Footer from './components/footer.js';
 import WildList from './components/wildList.js';
 import Header from './components/header.js';
+import Pokedex from './components/pokedex.js';
 
 customElements.define('main-footer', Footer);
 customElements.define('wild-list', WildList);
 customElements.define('main-header', Header);
+customElements.define('main-card',Pokedex);
--- atelier-pokedex/assets/js/components/header.js
+++ atelier-pokedex/assets/js/components/header.js
@@ -2,24 +2,24 @@ export default class Header extends HTMLElement {
   connectedCallback() {
     const current = this.getAttribute('current');
     this.innerHTML = `<header>
-      <div class="logo"><img src="public/img/logo-banner.png" alt="logo pokemon"></div>
-      <nav>
-        <ul>
-            <li><a style="color: ${
-              current === 'world' ? '#ffcb08' : 'white'
-            }" href="./">World</a></li>
-            <li><a style="color: ${
-              current === 'pokedex' ? '#ffcb08' : 'white'
-            }" href="./pokedex.html">Pokedex</a></li>
-            <li><a style="color: ${
-              current === 'history' ? '#ffcb08' : 'white'
-            }" href="./history.html">Historique</a></li>
-        </ul>
-      </nav>
-      <div class="counter">
-          <p class="catch">50</p>
-          <p class="notCatch">20</p>
-      </div>
-      </header>`;
+                        <div class="logo"><img src="./public/img/logo-banner.png" alt="logo pokemon"></div>
+                        <nav>
+                          <ul>
+                              <li><a style="color: ${
+                                current === 'world' ? '#ffcb08' : 'white'
+                              }" href="./">World</a></li>
+                              <li><a style="color: ${
+                                current === 'pokedex' ? '#ffcb08' : 'white'
+                              }" href="./pokedex.html">Pokedex</a></li>
+                              <li><a style="color: ${
+                                current === 'history' ? '#ffcb08' : 'white'
+                              }" href="./history.html">Historique</a></li>
+                          </ul>
+                        </nav>
+                        <div class="counter">
+                            <p class="catch">50</p>
+                            <p class="notCatch">20</p>
+                        </div>
+                      </header>`;
   }
 }
