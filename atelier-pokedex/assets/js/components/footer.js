ecb337018ff95b820b0cd3d6db99e5dd19eb71df
Coudji <106971118+Coudji@users.noreply.github.com>
2023-12-02 19:31:44 +0100

    Add capture & switch user from db to localStorage

--- atelier-pokedex/assets/js/components/footer.js
+++ atelier-pokedex/assets/js/components/footer.js
@@ -53,27 +53,68 @@ export default class Footer extends HTMLElement {
   }
 
   async renderUsers() {
-    const users = await fetch('http://localhost:3000/users').then((response) =>
+    /* const users = await fetch('http://localhost:3000/users').then((response) =>
       response.json().then((data) => {
         return data
           .map((user) => {
             return `<a href="#" class="userCard" data-username="${user.id}">
                       <img src="${user.img}" alt="${user.name}"/>
                       <h4>${user.name}</h4>
-                      <span class="nbrPkm">Pokemon attrapés : 10</span>
+                      <span class="nbrPkm">Pokemon possédés : ${user.total}</span>
                   </a>`;
           })
           .join('');
       }),
-    );
+    ); */
+    const isUsersExist = JSON.parse(localStorage.getItem('users'));
+    if(!isUsersExist){
+      const mockUsers = [
+        {
+          "name": "Amaury",
+          "catch": 0,
+          "escape": 0,
+          "total": 0,
+          "id": 1,
+          "img": "./public/img/bulbizarre.png"
+        },
+        {
+          "name": "Steve",
+          "catch": 0,
+          "escape": 0,
+          "total": 0,
+          "id": 2,
+          "img": "./public/img/salameche.png"
+        },
+        {
+          "name": "Amand",
+          "catch": 0,
+          "escape": 0,
+          "total": 0,
+          "id": 3,
+          "img": "./public/img/carapuce.png"
+        }
+      ]
+      localStorage.setItem('users', JSON.stringify(mockUsers));
+    }
+    const usersData = JSON.parse(localStorage.getItem('users'));
+    const users = usersData.map((user) => {
+      return `
+      <a href="#" class="userCard" data-username="${user.id}">
+        <img src="${user.img}" alt="${user.name}"/>
+        <h4>${user.name}</h4>
+        <span class="nbrPkm">Pokemon possédés : ${user.total}</span>
+      </a>`;
+    }).join('');
 
     this.innerHTML = `
-          <footer>
-              <div class="users">
-                  ${users}   
-              </div>
-          </footer>`;
+    <footer>
+        <div class="users">
+            ${users}   
+        </div>
+    </footer>`;
 
+    const checkActiveUser = localStorage.getItem('activeUser');
+    if(!checkActiveUser) localStorage.setItem('activeUser',1);
     const activeUser = localStorage.getItem('activeUser');
     if (activeUser) {
       const activeUserCard = this.querySelector(
--- atelier-pokedex/assets/js/main.js
+++ atelier-pokedex/assets/js/main.js
@@ -40,51 +40,111 @@ export function swapStorage(){
     generateWildList()
 }
 
-export function displayCaptureModal(event){
+export function displayCaptureModal(event) {
     const pkmIndex = event.target.parentNode.getAttribute('index');
     const pkmData = JSON.parse(localStorage.getItem('wildList'))[pkmIndex];
     const body = document.querySelector('body');
+  
+    const layer = document.createElement('div');
+    layer.classList.add('layer');
+    layer.addEventListener('click', (event) => {
+      if (!event.target.closest('.captureModal')) {
+        layer.remove();
+      }
+    });
+  
     const modalContainer = document.createElement('div');
     modalContainer.classList.add('captureModal');
-    modalContainer.setAttribute('index',`${pkmIndex}`);
     const captureMarkUp = `<p>Un ${pkmData.name} sauvage apparait !</p>
-    <div><img src="${pkmData.image}"</div>
-    <p id="modal-msg">Voulez-vous tenter de le capturer ?</p>
-    <div>
-        <button id="btn-back" class="fa-solid"><i class="fa-solid fa-circle-arrow-left"></i></button>
-        <button id="btn-capture"><img src="./public/img/pokeball.png" alt="Capturer Pokemon"></button>
-    </div>`
-
-    modalContainer.innerHTML= captureMarkUp;
-    body.appendChild(modalContainer);
-
-    document.getElementById('btn-back').addEventListener('click',(event)=>{
-        event.target.closest('.captureModal').remove();
-    })
-
-    document.getElementById('btn-capture').addEventListener('click',()=>{
-        capturePokemon(pkmIndex, pkmData);
-    })
+      <div><img src="${pkmData.image}"</div>
+      <p id="modal-msg">Voulez-vous tenter de le capturer ?</p>
+      <div>
+          <button id="btn-back" class="fa-solid"><i class="fa-solid fa-circle-arrow-left"></i></button>
+          <button id="btn-capture"><img src="./public/img/pokeball.png" alt="Capturer Pokemon"></button>
+      </div>`;
+    modalContainer.innerHTML = captureMarkUp;
+    body.appendChild(layer);
+    layer.appendChild(modalContainer);
+    
+    document.getElementById('btn-back').addEventListener('click', (event) => {
+      layer.remove();
+    });
+    document.getElementById('btn-capture').addEventListener('click', () => {
+      capturePokemon(pkmData,pkmIndex);
+    });
 }
 
-function capturePokemon(event,pkmIndex,pkmData){
-    const userId = "";
+function capturePokemon(pkmData,pkmIndex){
+    const row = document.querySelector(`[index="${pkmIndex}"]`);
+    const modalMsg = document.getElementById('modal-msg');
+    const userId = Number(localStorage.getItem('activeUser'));
     const pokemon = pkmData;
-    getOneUser(3)
+    const userData = getOneUser(userId);
+    const isCaptured = calculateCaptureChance(pokemon,userData);
 
+    if(isCaptured === false){
+        row.remove();
+        modalMsg.innerText = `${pokemon.name} prend la fuite`;
+        modalMsg.style.backgroundColor = "red";
+        userData.escape +=1;
+        updateOneUser(userId, userData);
+    }else{
+        row.remove()
+        modalMsg.innerText = `Vous avez capturé ${pokemon.name}`;
+        modalMsg.style.backgroundColor = "green";
+        pokemon.ownerId = Number(userId);
+        userData.total +=1;
+        userData.catch +=1;
+
+        fetch('http://localhost:3000/pokedex',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(pokemon)
+        });
+
+        updateOneUser(userId, userData);
+    }
+    
 }
 
-function getOneUser(id){
-    const user = fetch(`http://localhost:3000/users/${id}`)
-    .then((response)=> response.json())
-    .then((data) =>{
-        console.log(data);
-    })
+function calculateCaptureChance(pokemon,userData){
+    const dice = randomNumber(20)
+    const speed = pokemon.stats.speed
+    const bonus = Math.floor(Math.log2(userData.catch / 10));
+    let result = ''
+    let capture =''
+
+    if(userData.catch >= 10){
+        result = dice - (speed/10) + bonus
+    }else{
+        result = dice - (speed/10)
+    }
+
+    if(result > 0){
+        capture = true
+    }else{
+        capture = false
+    }
+
+    return capture
+}
+
+function getOneUser(id) {
+    const usersData = JSON.parse(localStorage.getItem('users')) || [];
+    const user = usersData.find(u => u.id === id);
+    return user;
+}
+
+function updateOneUser(id, updatedData) {
+    const usersData = JSON.parse(localStorage.getItem('users')) || [];
+    const updatedUsers = usersData.map(user => (user.id === id ? { ...user, ...updatedData } : user));
+    localStorage.setItem('users', JSON.stringify(updatedUsers));
 }
 
 export function handleUserSelectedEvent(event) {
     const activeUser = event.detail;
-    console.log('Utilisateur actif dans main.js :', activeUser);
   
     const userHeader = document.getElementById('activeUserDisplay');
     userHeader.innerHTML = `
