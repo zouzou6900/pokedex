d5ae07a1491103240402de323153cbda64f158b6
zouzou6900 <meersman_amaury@hotmail.com>
2023-12-02 13:44:16 +0100

    mise a jour pour rapatrier le main

--- atelier-pokedex/assets/js/main.js
+++ atelier-pokedex/assets/js/main.js
@@ -1,4 +1,3 @@
-
 export function randomNumber(max){
     return Math.floor(Math.random() * (max + 1));
 }
@@ -51,10 +50,46 @@ export function displayCaptureModal(event){
     <div><img src="${pkmData.image}"</div>
     <p id="modal-msg">Voulez-vous tenter de le capturer ?</p>
     <div>
-        <button id="btn-back">btnback</button>
-        <button id="btn-capture">btncapture</button>
+        <button id="btn-back" class="fa-solid"><i class="fa-solid fa-circle-arrow-left"></i></button>
+        <button id="btn-capture"><img src="./public/img/pokeball.png" alt="Capturer Pokemon"></button>
     </div>`
 
     modalContainer.innerHTML= captureMarkUp;
     body.appendChild(modalContainer);
+
+    document.getElementById('btn-back').addEventListener('click',(event)=>{
+        event.target.closest('.captureModal').remove();
+    })
+
+    document.getElementById('btn-capture').addEventListener('click',()=>{
+        capturePokemon(pkmIndex, pkmData);
+    })
 }
+
+function capturePokemon(event,pkmIndex,pkmData){
+    const userId = "";
+    const pokemon = pkmData;
+    getOneUser(3)
+
+}
+
+function getOneUser(id){
+    const user = fetch(`http://localhost:3000/users/${id}`)
+    .then((response)=> response.json())
+    .then((data) =>{
+        console.log(data);
+    })
+}
+
+export function handleUserSelectedEvent(event) {
+    const activeUser = event.detail;
+    console.log('Utilisateur actif dans main.js :', activeUser);
+  
+    const userHeader = document.getElementById('activeUserDisplay');
+    userHeader.innerHTML = `
+      Salut ${activeUser}`;
+  }
+  
+  document
+    .querySelector('main-footer')
+    .addEventListener('userSelected', handleUserSelectedEvent);
